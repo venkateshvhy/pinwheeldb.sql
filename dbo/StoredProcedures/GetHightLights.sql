de81c387095c72668123860803fe04ea5845996f
venkateshvhy <venkatesh.b@vhypotenuse.com>
2025-01-20 14:08:27 +0530

    fix

--- dbo/StoredProcedures/GetHightLights.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[GetHightLights]
-AS
-BEGIN
-Set NoCount On
-select * from Highlights;
-END;
--- dbo/StoredProcedures/GetHightlightProperties.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[GetHightlightProperties]
-@HighlightId INT
-AS
-BEGIN
-Set NoCount On
-select * from HightlightsProperties hp
-where hp.HighlightId = @HighlightId;
-END
